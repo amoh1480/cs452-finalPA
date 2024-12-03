5f58338de8950c9cd3893cbdd592bb1c1846acc9
masinoex <masinoex@dukes.jmu.edu>
2024-12-03 10:41:16 -0500

    notes on solutions

---
+++ code_solution/approx_elvis.py
@@ -0,0 +1,28 @@
+################################################
+# Deterministic
+# assign arbitrary order to the edges
+
+# V = {}
+
+# foreach (edge e in edgeList)
+
+#   if both vertices are not taken
+
+#     take both vertices and remove incident edges
+
+#   else ignore both vertices
+
+# VC = V
+################################################
+# Probablistic
+# assign arbitrary order to the edges
+
+# V = {}
+
+# foreach (edge e in edgeList)
+
+#   take one endpoint at random
+
+#   Remove edges incident to the chosen endpoint
+
+# VC = V
