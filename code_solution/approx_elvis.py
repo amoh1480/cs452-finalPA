ee76f0a2effbe5d59ed0cacaa40cca7c4d76a2b0
masinoex <masinoex@dukes.jmu.edu>
2024-12-04 12:52:37 -0500

    something that sort of works

--- code_solution/approx_elvis.py
+++ code_solution/approx_elvis.py
@@ -26,3 +26,57 @@
 #   Remove edges incident to the chosen endpoint
 
 # VC = V
+
+
+# https://www.geeksforgeeks.org/searching-algorithms-for-a-2d-arrays-matrix/
+# Didn't really work for removing due to decreasing array size but idea came
+# from this site
+def removeElement (arr, target):
+    i = 0
+    while i < len(arr): 
+        for j in range(len(arr[i])):
+            if arr[i][j] == target:
+                print("deleted: ", arr[i])
+                del arr[i]
+                i -= 1 
+                break # Need this 
+        i += 1
+    return
+
+
+def vertex_cover_prob(graph):
+    V = []
+    
+    for edge in graph: 
+        V.append(edge[0])
+        V.append(edge[1])
+        removeElement(graph, edge[0])
+        removeElement(graph, edge[1])
+        print(graph)
+    
+    return V
+
+
+def main():
+    # Read the input
+    data = []
+    num_edges = input("Number of edges: ")
+    while True:
+        try:
+            # Read a line of input
+            line = input().strip()
+            if not line:  # Stop if input is empty
+                break
+            data.append(list(map(str, line.split())))
+        except EOFError:  # Stop on EOF
+            break
+    print(num_edges)
+    print(data)
+    
+    min_vertex = vertex_cover_prob(data)
+    print(min_vertex)
+    
+    return
+
+if __name__ == '__main__':
+    main()
